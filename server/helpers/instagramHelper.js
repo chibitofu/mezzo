3d41aa90d3156440ea808a6b0ae00d2555ece59d
chibitofu <erinmoony@gmail.com>
2016-02-08 13:29:51 -0800

    Styling updates.

--- server/helpers/instagramHelper.js
+++ server/helpers/instagramHelper.js
@@ -13,45 +13,45 @@ module.exports = {
 
     var findTag = instagramApiStart + tagName + instagramApiEnd + apiKey + count;
 
-    // request(findTag, function(req, res){
-    //   var tags = JSON.parse(res.body);
-    //   console.log(tags);
-    //   callback({tags: tags})
-    // });
-
     request(findTag, function(req, res){
-      var tags= JSON.parse(res.body).data;
-      for (var i = 0; i < tags.length; i++) {
-        tagsFull.tags.push(tags[i]);
-      }
-    }).then(function(resultOne){
-      var tags = JSON.parse(resultOne);
-      if (JSON.parse(resultOne).pagination) {
-        var pagination = JSON.parse(resultOne).pagination;
-        request(pagination.next_url, function(req, res) {
-          var tags = JSON.parse(res.body).data;
-          for (var i = 0; i < tags.length; i++) {
-            tagsFull.tags.push(tags[i]);
-          }
-        }).then(function(resultTwo){
-          var tags = JSON.parse(resultTwo);
-          if (JSON.parse(resultTwo).pagination) {
-            var pagination = JSON.parse(resultTwo).pagination;
-            request(pagination.next_url, function(req, res) {
-              var tags = JSON.parse(res.body).data;
-              for (var i = 0; i < tags.length; i++) {
-                tagsFull.tags.push(tags[i]);
-              }
-              callback({tagsFull: tagsFull})
-            });
-          } else {
-            callback({tagsFull: tagsFull})
-          }
-        });
-      } else {
-        callback({tagsFull: tagsFull})
-      }
+      var tags = JSON.parse(res.body);
+      console.log(tags);
+      callback({tags: tags})
     });
+
+    // request(findTag, function(req, res){
+    //   var tags= JSON.parse(res.body).data;
+    //   for (var i = 0; i < tags.length; i++) {
+    //     tagsFull.tags.push(tags[i]);
+    //   }
+    // }).then(function(resultOne){
+    //   var tags = JSON.parse(resultOne);
+    //   if (JSON.parse(resultOne).pagination) {
+    //     var pagination = JSON.parse(resultOne).pagination;
+    //     request(pagination.next_url, function(req, res) {
+    //       var tags = JSON.parse(res.body).data;
+    //       for (var i = 0; i < tags.length; i++) {
+    //         tagsFull.tags.push(tags[i]);
+    //       }
+    //     }).then(function(resultTwo){
+    //       var tags = JSON.parse(resultTwo);
+    //       if (JSON.parse(resultTwo).pagination) {
+    //         var pagination = JSON.parse(resultTwo).pagination;
+    //         request(pagination.next_url, function(req, res) {
+    //           var tags = JSON.parse(res.body).data;
+    //           for (var i = 0; i < tags.length; i++) {
+    //             tagsFull.tags.push(tags[i]);
+    //           }
+    //           callback({tagsFull: tagsFull})
+    //         });
+    //       } else {
+    //         callback({tagsFull: tagsFull})
+    //       }
+    //     });
+    //   } else {
+    //     callback({tagsFull: tagsFull})
+    //   }
+    // });
   }
 
 };
